bd2ff9472227b383259b9ef221200936580593d7
Nicrueda <164514047+nicktechz@users.noreply.github.com>
2024-07-26 08:07:49 -0500

    Added a JS function to don't allow whitespaces on email input

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -1,6 +1,7 @@
 const passwordInput = document.getElementById("password");
 const seePasswordBtn = document.getElementById("seePassword");
 const seePasswordIcon = document.getElementById("seePasswordIcon");
+const emailInput = document.getElementById("email");
 
 seePasswordBtn.addEventListener("click", () => {
   if (passwordInput.type == "password") {
@@ -18,4 +19,12 @@ passwordInput.addEventListener("keypress", (e) => {
   }
   const updatedInput = passwordInput.value.replace(/\s/g, "");
   passwordInput.value = updatedInput;
+});
+
+emailInput.addEventListener("keypress", (e) => {
+  if (e.code === "Space") {
+    e.preventDefault();
+  }
+  const updatedInput = emailInput.value.replace(/\s/g, "");
+  emailInput.value = updatedInput;
 });
